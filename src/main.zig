b593e837d06caec5726402a9f892c67bc860f793
Jordan Baron <jordannbaron@gmail.com>
2024-07-07 17:20:51 -0400

    write to connection

--- src/main.zig
+++ src/main.zig
@@ -4,9 +4,7 @@ const net = std.net;
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
-
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    try stdout.print("Logs from your program will appear here!\n", .{});
+    // try stdout.print("Logs from your program will appear here!\n", .{});
 
     const address = try net.Address.resolveIp("127.0.0.1", 4221);
     var listener = try address.listen(.{
@@ -14,6 +12,8 @@ pub fn main() !void {
     });
     defer listener.deinit();
 
-    _ = try listener.accept();
+    const connection = try listener.accept();
+    connection.stream.write("HTTP/1.1 200 OK\r\n\r\n");
+
     try stdout.print("client connected!", .{});
 }
