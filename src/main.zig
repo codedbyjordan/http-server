a974fed7ec6e012e1ee11377014f3ca2f3d32a18
Jordan Baron <jordannbaron@gmail.com>
2024-07-07 23:55:36 -0400

    echo endpoint

--- src/main.zig
+++ src/main.zig
@@ -6,12 +6,14 @@ const net = std.net;
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     // try stdout.print("Logs from your program will appear here!\n", .{});
-
-    const address = try net.Address.resolveIp("127.0.0.1", 4221);
+    const port = 4221;
+    const address = try net.Address.resolveIp("127.0.0.1", port);
     var listener = try address.listen(.{
         .reuse_address = true,
     });
     defer listener.deinit();
+
+    stdout.print("Listening on port {}\n", .{port}) catch unreachable;
 
     const connection = try listener.accept();
 
@@ -25,10 +27,13 @@ pub fn main() !void {
     var splitBytes = mem.splitAny(u8, buffer[0..bytesRead], " ");
 
     _ = splitBytes.next();
-    const resource = splitBytes.next();
+    const endpoint = splitBytes.next() orelse return;
 
-    if (mem.eql(u8, resource.?, "/")) {
+    if (mem.eql(u8, endpoint, "/")) {
         _ = try connection.stream.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else if (mem.startsWith(u8, endpoint, "/echo")) {
+        const echoParameter = endpoint[6..];
+        try std.fmt.format(connection.stream.writer(), "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {d}\r\n\r\n{s}", .{ echoParameter.len, echoParameter });
     } else {
         _ = try connection.stream.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
